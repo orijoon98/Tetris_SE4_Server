1260e46cb20660a09ceadd8df482502fb087662b
HyeokJoon Kong <orijoon98@gmail.com>
2022-04-08 20:16:58 +0900

    Initial commit

---
+++ src/test/java/se4/tetris/TetrisApplicationTests.java
@@ -0,0 +1,13 @@
+package se4.tetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
